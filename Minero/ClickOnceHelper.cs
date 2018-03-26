1e56b245a7793160b405823cea37f6016584765b
Ian Escarro <ian.escarro@gmail.com>
2018-03-26 09:19:50 +0800

    Add initial ClickOnce helper to add in Startup

---
+++ Minero/ClickOnceHelper.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Minero
+{
+    public static class ClickOnceHelper
+    {
+        public static void AddShortcutToStartupGroup(string publisherName, string productName)
+        {
+//            if (ApplicationDeployment.IsNetworkDeployed &&
+//                ApplicationDeployment.CurrentDeployment.IsFirstRun) {
+//
+//                string startupPath = Environment.GetFolderPath(Environment.SpecialFolder.Startup);
+//                startupPath = Path.Combine(startupPath, productName) + ".appref-ms";
+//                if (!File.Exists(startupPath)) {
+//                    string allProgramsPath = Environment.GetFolderPath(Environment.SpecialFolder.Programs);
+//                    string shortcutPath = Path.Combine(allProgramsPath, publisherName);
+//                    shortcutPath = Path.Combine(shortcutPath, productName) + ".appref-ms";
+//                    File.Copy(shortcutPath, startupPath);
+//                }
+//            }
+        }
+    }
+}
